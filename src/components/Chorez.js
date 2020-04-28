82b168e517442c800c02ea6a8f5adc28f6411fe7
blake slochowsky <blakeslochowsky.com>
2020-04-28 16:11:28 -0500

    basic login regester

---
+++ src/components/Chorez.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react"
+import Dashboard from "./Dashboard"
+import { Auth } from "./auth/Auth"
+
+export default () => {
+    const [check, update] = useState(false)
+    const toggle = () => update(!check)
+
+    return (
+        localStorage.getItem("Chorez_customer") ? <Dashboard /> : <Auth toggle={toggle} />
+    )
+}
---
+++ src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+
+
+export default (props) => {
+
+    return (
+        <>
+            <h1>Chorez</h1>
+            
+        </>
+    )
+}
